5c877aa4d35aed89ab2d200f546158d46f309f08
SABINO KARI B <202100977b@utea.edu.pe>
2023-10-13 07:42:57 -0500

    app_dhared

---
+++ lib/widgets/my_drawer_widget.dart
@@ -0,0 +1,76 @@
+
+import 'package:flutter/material.dart';
+
+class MyDrawerWitget extends StatelessWidget {
+  const MyDrawerWitget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      "https://images.pexels.com/photos/18515329/pexels-photo-18515329/free-photo-of-oscuro-textura-abstracto-diseno.jpeg"),
+                ),
+              ),
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.amber,
+                      backgroundImage: NetworkImage(
+                          "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg"),
+                    ),
+                    Text(
+                      "Fiorella Rojas Padilla",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "Administrador",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.people),
+              title: Text("Mi Perfil"),
+            ),
+            ListTile(
+              leading: Icon(Icons.file_copy),
+              title: Text("Portafolio"),
+            ),
+            ListTile(
+              leading: Icon(Icons.lock_clock),
+              title: Text("Cambiar Contraseña"),
+            ),
+            Divider(
+              indent: 12,
+              endIndent: 12,
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text("Salir"),
+            ),
+          ],
+        ),
+      );
+  }
+}
